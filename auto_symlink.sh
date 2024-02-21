4f77e3157f969692d058b7ae3ccef9c5e383c94f
reyuki <hikikomori.yukisan@gmail.com>
2024-02-21 17:07:08 +0700

    update script

--- auto_symlink.sh
+++ auto_symlink.sh
@@ -11,6 +11,8 @@ chmod -v +x $absolute_path/screenshot_select.sh
 ln -vsdf $absolute_path/nvim ~/.config/nvim
 ln -vsf $absolute_path/.bashrc ~/.bashrc
 ln -vsf $absolute_path/.inputrc ~/.inputrc
+ln -vsf $absolute_path/.Xresources ~/.Xresources
+ln -vsf $absolute_path/.xinitrc ~/.xinitrc
 sudo ln -vsf $absolute_path/hosts /etc/hosts
 # sudo ln -vsdf $absolute_path/download_list/download.sh /usr/local/bin/download.sh
 sudo ln -vsf $absolute_path/screenshot.sh /usr/local/bin/screenshot.sh
