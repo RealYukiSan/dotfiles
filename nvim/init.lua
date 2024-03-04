f9590c5563aacbf522bb2ae17412e813dd664dce
reyuki <hikikomori.yukisan@gmail.com>
2024-03-04 21:41:54 +0700

    refactor ftplugin and migrate to lspconfig and add plugin: emmet_ls, nvim-cmp, etc etc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
+-- see :help normal-index to avoid conflict on mapping keybinding
+-- and :help keycodes to see the meaning of symbol
+-- https://www.reddit.com/r/vim/comments/6388fz/comment/dfs2s4o/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+-- see loaded vim or lua file by :scriptnames
+
 local opts = { noremap=true, silent=true }
 
 vim.g.mapleader = ","
@@ -22,3 +27,61 @@ end
 vim.keymap.set('n', '<Leader>d', open_float, opts)
 
 vim.opt.foldmethod = "indent"
+
+local function goto()
+	vim.diagnostic.goto_next(nil)
+end
+
+-- for goto file use gf or <C-]> (jump to a subject), goto link website use gx
+vim.keymap.set('v', '<C-g>', goto, opts)
+
+vim.keymap.set('v', '<C-y>', '"+y', opts)
+vim.keymap.set('v', '<C-p>', '"+gP', opts)
+
+vim.cmd('packadd nvim-cmp')
+
+local cmp = require'cmp'
+local lsp = require'lspconfig'
+local snippet = require'luasnip'
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+cmp.setup({
+	snippet = {
+		expand = function (args)
+			snippet.lsp_expand(args.body)
+		end
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'buffer' }
+	}),
+	mapping = cmp.mapping.preset.insert({
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<C-k>'] = cmp.mapping.scroll_docs(4),
+		['<C-j>'] = cmp.mapping.scroll_docs(-4),
+		['<CR>'] = cmp.mapping.confirm({ select = true })
+	})
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- unlike clangd, emmet_ls seems not support omnifunc for the auto-completion, therefore we need to utilize it with nvim-cmp
+lsp['emmet_ls'].setup {
+	cmd = { 'emmet-ls', '--stdio' },
+	filetypes = {
+		"css",
+		"html"
+	},
+	capabilities = capabilities,
+}
+
+lsp['clangd'].setup({
+	filetypes = { 'c' },
+	cmd = {'clangd'},
+})
---
+++ nvim/install.lua
@@ -0,0 +1,13 @@
+-- install the dependencies plugin/package by :luafile install.lua
+vim.cmd([[
+packadd minpac
+
+call minpac#init({ 'verbose': 4 })
+call minpac#add('k-takata/minpac', {'type': 'opt'})
+call minpac#add('neovim/nvim-lspconfig', {'do': '!make'})
+call minpac#add('hrsh7th/nvim-cmp', { 'type': 'opt', 'do': '!make'})
+call minpac#add('hrsh7th/cmp-nvim-lsp', {'do': '!make'})
+call minpac#add('hrsh7th/cmp-buffer', {'do': '!make'})
+call minpac#add('L3MON4D3/LuaSnip', {'do': '!make'})
+call minpac#update()
+]])
